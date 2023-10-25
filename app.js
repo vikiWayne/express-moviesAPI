26cda8c92a87932985324e8170034594cb90aa13
Vaisakh <vaisakhpu007@gmail.com>
2023-10-25 23:07:45 +0530

    added CORS headers

--- app.js
+++ app.js
@@ -8,9 +8,24 @@ app.use(express.json());
 app.use(express.static('./public'));
 
 const loggerService = (req, res, next) => {
-    console.log('Request received to /movie');
+    console.log(`Request received to ${req.originalUrl}`);
     next();
 };
+
+// CORS headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+
+    next();
+});
 
 // GET - api/movies
 app.use('/api/v1.0', loggerService, v1Routes);
